da78c16f226d7dbc4095e8718c91c55c24048827
Nicholas Kallas <kallasnicholas@gmail.com>
2022-01-15 03:28:11 -0600

    Allows user to enter a book's title, author, number of pages, and if they've read it. After clicking add book, it is stored in their library.

---
+++ script.js
@@ -0,0 +1,63 @@
+"use strict";
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${title} by ${author}, ${pages} pages, ${read}`;
+  };
+}
+
+// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+// theHobbit.info();
+// console.log(theHobbit.info());
+
+function addBookToLibrary() {
+  let newBook = new Book();
+  newBook.title = document.getElementById("titleInput").value;
+  newBook.author = document.getElementById("authorInput").value;
+  newBook.pages = document.getElementById("pagesInput").value;
+  if (document.getElementById("readInput").checked) {
+    newBook.read = "Read";
+  } else {
+    newBook.read = "Not read";
+  }
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+  displayBooks();
+}
+
+function displayBooks() {
+  clearCards();
+  for (let i = 0; i < myLibrary.length; i++) {
+    if (myLibrary[i].title === "") {
+      myLibrary.shift();
+    }
+    const card = document.createElement("div");
+    card.setAttribute("style", "width: 20rem height: 20rem");
+    const titleDisplay = document.createElement("h2");
+    const authorDisplay = document.createElement("p");
+    const pagesDisplay = document.createElement("p");
+    const readStatus = document.createElement("p");
+    const removeBtn = document.createElement("button");
+    titleDisplay.textContent = myLibrary[i].title;
+    authorDisplay.textContent = myLibrary[i].author;
+    pagesDisplay.textContent = myLibrary[i].pages;
+    readStatus.textContent = myLibrary[i].read;
+    removeBtn.textContent = "Remove Book";
+    document.querySelector("#card-container").appendChild(card);
+    card.appendChild(titleDisplay);
+    card.appendChild(authorDisplay);
+    card.appendChild(pagesDisplay);
+    card.appendChild(readStatus);
+    card.appendChild(removeBtn);
+  }
+}
+
+function clearCards() {
+  document.querySelector("#card-container").textContent = "";
+}
