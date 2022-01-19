d97d5cc5fcefd11a185d07595a5b836c0dc5234b
Nicholas Kallas <kallasnicholas@gmail.com>
2022-01-18 21:55:45 -0600

    Add book window now closes any time a user clicks outside of the content

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 "use strict";
 
+// empty array to store books in
 let myLibrary = [];
 
+// object constructor that will be used to create all of the books
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -12,6 +14,7 @@ function Book(title, author, pages, read) {
   };
 }
 
+// function that can be used on all instances of Book because it was declared on Book's prototype
 Book.prototype.toggle = function () {
   if (this.read === "Read") {
     this.read = "Not read";
@@ -20,8 +23,7 @@ Book.prototype.toggle = function () {
   }
 };
 
-console.log(Book.prototype);
-
+// function that creates a new Book (new instance of Book), gives the book a unique ID, pushes the book to the myLibrary array, and runs the displayBooks function
 function addBookToLibrary() {
   let newBook = new Book();
   newBook.title = document.getElementById("title-input").value;
@@ -34,10 +36,10 @@ function addBookToLibrary() {
     newBook.read = "Not read";
   }
   myLibrary.push(newBook);
-  console.log(myLibrary);
   displayBooks();
 }
 
+// function that runs the clearCards function (makes the card container empty) and loops through the myLibrary array, giving the obect properties DOM elements
 function displayBooks() {
   clearCards();
   for (let i = 0; i < myLibrary.length; i++) {
@@ -65,18 +67,17 @@ function displayBooks() {
       "font-size: 2.4rem; margin-top: 1rem; margin-bottom: 1rem;"
     );
     const readStatusBtn = document.createElement("button");
-    readStatusBtn.setAttribute("id", myLibrary[i].id);
+    readStatusBtn.setAttribute("id", myLibrary[i].id); // gives the readStatusBtn the same ID as the book that it is on in the DOM. This associtaes the readStatusBtn with the book.
     readStatusBtn.setAttribute(
       "style",
       "background-color: #06beb6; color: #fff; border: none; font-size: 1.8rem; width: 10rem; margin-top: 1rem; padding-top: 1rem; padding-bottom: 1rem; margin-bottom: 1rem; border-radius: 0.5rem;"
     );
     readStatusBtn.addEventListener("click", () => {
-      console.log(readStatusBtn.id);
       const selectedBook = myLibrary.find(
         (book) => book.id === readStatusBtn.id
-      );
-      selectedBook.toggle();
-      displayBooks();
+      ); // looks through the myLibrary array to find a book that shares the same ID as the readStatusBtn and gives this book a variable of selectedBook
+      selectedBook.toggle(); // calls the toggle() function from the Book constructor's prototype to change the readStatus
+      displayBooks(); // displays the books again so that the DOM can be updated with the new read status
     });
     readStatusBtn.addEventListener("mouseover", () => {
       readStatusBtn.classList.add("book-card-btn-hover");
@@ -85,7 +86,7 @@ function displayBooks() {
       readStatusBtn.classList.remove("book-card-btn-hover");
     });
     const removeBtn = document.createElement("button");
-    removeBtn.setAttribute("id", myLibrary[i].id);
+    removeBtn.setAttribute("id", myLibrary[i].id); // gives the remove button the same ID as the current book to associate them
     removeBtn.setAttribute(
       "style",
       "background-color: #06beb6; color: #fff; border: none; font-size: 1.8rem; width: 10rem; margin-top: 1rem; margin-bottom: 1rem; padding-top: 1rem; padding-bottom: 1rem; border-radius: 0.5rem;"
@@ -96,7 +97,7 @@ function displayBooks() {
     removeBtn.addEventListener("mouseout", () => {
       removeBtn.classList.remove("book-card-btn-hover");
     });
-    removeBtn.addEventListener("click", removeBook);
+    removeBtn.addEventListener("click", removeBook); // removeBook() function is called when the removeBtn is clicked
     titleDisplay.textContent = myLibrary[i].title;
     authorDisplay.textContent = myLibrary[i].author;
     pagesDisplay.textContent = `${myLibrary[i].pages} pages`;
@@ -113,14 +114,13 @@ function displayBooks() {
 }
 
 function clearCards() {
-  document.querySelector("#card-container").textContent = "";
+  document.querySelector("#card-container").textContent = ""; // clears all of the cards from the DOM
 }
 
 function removeBook() {
-  console.log(this.id);
-  const newArray = myLibrary.filter((book) => book.id !== this.id);
-  myLibrary = newArray;
-  displayBooks();
+  const newArray = myLibrary.filter((book) => book.id !== this.id); // Creates a new array of books that don't have the same ID as the removeBookBtn
+  myLibrary = newArray; // sets myLibrary array equal to the newArray
+  displayBooks(); // displays the books again to update the DOM
 }
 
 function uuidv4() {
@@ -130,7 +130,7 @@ function uuidv4() {
       (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
     ).toString(16)
   );
-}
+} // sets a unique ID for each book
 
 const addBookFormBtn = document.querySelector(".add-book-form-btn");
 addBookFormBtn.addEventListener("click", () => {
@@ -140,11 +140,12 @@ addBookFormBtn.addEventListener("click", () => {
 });
 
 // modal
+const modal = document.querySelector(".modal");
 const modalDisplay = document.querySelector(".hide-show-modal");
 const addBookBtn = document.querySelector("#add-book-btn");
 const closeModalBtn = document.querySelector(".book-form-close-btn");
-console.log(closeModalBtn);
 
+// function to close modal if outside click
 addBookBtn.addEventListener("click", openModal);
 function openModal() {
   modalDisplay.style.display = "block";
@@ -154,6 +155,15 @@ closeModalBtn.addEventListener("click", closeModal);
 function closeModal() {
   modalDisplay.style.display = "none";
 }
+
+// listens for click outside the modal content
+window.addEventListener("click", outsideClick);
+function outsideClick(e) {
+  if (e.target === modal) {
+    modalDisplay.style.display = "none";
+  }
+}
+
 function clearForm() {
   document.querySelector("form").reset();
 }
